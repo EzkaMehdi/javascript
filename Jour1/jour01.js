0e8e1913fd4fcc08c36eece62341ddd307ed2c7a
Mehdi <ezka.mehdi@gmail.com>
2021-03-08 16:23:25 +0100

    java

---
+++ Jour1/jour01.js
@@ -0,0 +1,34 @@
+//console.log("Hello world !");1
+
+//var test = "My name is Kobe Bryant";2
+//console.log(test);
+
+//var name = " Kobe";
+//console.log("nice to meet you " + name);3
+
+//var testlength = "i'm very long !";
+//console.log(testlength.length);4
+
+//var food = "croissant is mleh";
+//food = food.replace("mleh", "so good");
+
+//console.log(food); 5
+
+//var basic = "This is Cool";
+//var basicUp = basic.toUpperCase();
+//var basicDown = basic.toLowerCase();
+//console.log(basicUp + basicDown);6
+
+//var word = "banana";
+//var letters = word.split("")
+
+//console.log(letters);7
+
+//var age = 22
+//var template = "i'm ___ years old";
+
+//console.log("i'm " + age + " years old");8
+
+var changed = "Bonjour";
+changed = changed.replace(/o/g,"a");
+console.log(changed);
