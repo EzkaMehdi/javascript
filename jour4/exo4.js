328d8a4d52bffec25d77be36a1e067b8a9cb97df
Mehdi <ezka.mehdi@gmail.com>
2021-03-11 17:20:36 +0100

    javascript suite

---
+++ jour4/exo4.js
@@ -0,0 +1,11 @@
+function compare(num1,num2) {
+    if (num1 > num2){
+        console.log("num1 is bigger");
+    }
+    else (num2 > num1)
+    {
+     console.log("num2 is bigger");
+    }
+
+};
+compare();
---
+++ jour4/exo5.js
@@ -0,0 +1,6 @@
+function addUp(num) {
+    for (var i = 0; i <=num; i++) {  
+        
+      }
+   };
+   addUp();
