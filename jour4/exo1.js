42701ac6d6b3c2d88dbdb69cbc146e999b366081
Mehdi <ezka.mehdi@gmail.com>
2021-03-11 16:26:48 +0100

    exo jour 4 javascript

---
+++ jour4/exo1.js
@@ -0,0 +1,10 @@
+var cat =  {
+	name: "Garfield",
+	isCute: true,
+	age: 3,
+};
+console.log(cat);
+console.log(cat.age);
+if (isCute = true) {
+    console.log("So cute");
+}
---
+++ jour4/exo2.js
@@ -0,0 +1,13 @@
+var cat =  {
+	name: "Garfield",
+	isCute: true,
+	age: 3,
+};
+var cat2 =  {
+	name: "bizbiz",
+	isCute: false,
+	age: 21,
+};
+var cats = [cat , cat2];
+console.log(cat.age);
+console.log(cat2.isCute);
