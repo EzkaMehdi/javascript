52bad4eaa08677547dbb0b366cbab0344e155c82
Mehdi <ezka.mehdi@gmail.com>
2021-03-16 11:04:58 +0100

    exo 3 revisions

---
+++ Révisions/exo3.js
@@ -0,0 +1,13 @@
+//- Créez une fonction `checkPal` qui reçoit une string en paramètre
+//- La fonction retourne "Palindrome !" si le mot se lit dans les deux sens, sinon elle retourne "Nope"
+//- Vérifiez que vous obtenez un positif avec l'argument "racecar" mais pas avec "laptop"
+
+function checkPal(str){
+    if(str.split("").reverse().join("") === str){
+        console.log("Palindrome");
+    }
+    else{
+        console.log("Nope");
+    }
+}
+checkPal("girafarig");
---
+++ Révisions/exo4.js
@@ -0,0 +1,16 @@
+//- Créez une fonction `swap` qui reçoit une string en paramètre
+//- La fonction retourne une string avec la casse inverse : une majuscule deviendra minuscule et vice-versa
+//- Appelez votre fonction avec l'argument "Hello World" et vérifiez que vous obtenez "hELLO wORLD"
+
+//function swap(str){
+
+//}
+//swap("Hello World");
+
+//var test = "I'm normal"
+var str = 'Hello World !';
+ 
+
+str.toLowerCase(""); // hello world !
+
+console.log(str)
