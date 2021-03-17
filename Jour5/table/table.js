81e09f02abb498bfd3947992c538679128e17afb
Mehdi <ezka.mehdi@gmail.com>
2021-03-17 16:55:32 +0100

    exo 2

---
+++ Jour5/table/table.js
@@ -0,0 +1,33 @@
+// - Créez une fonction `multiply` qui recevra un paramètre (un nombre entier)
+// - Faites en sorte que la fonction affiche la table de multiplication de ce nombre dans la console (de 1 à 10) ligne par ligne
+// - Vous devrez donner l'argument dans le terminal en appelant le programme
+
+//     ⇒ Si vous ne donnez pas d'argument, le programme doit afficher "error" dans la console
+
+function multiply(number) {
+    var firstInput = parseFloat(number)
+    
+    if (Number.isInteger(firstInput) == false) {
+        console.log("error");
+    } else {
+        for (var i = 1; i <= 10; i++) {
+            console.log(firstInput, "x", i, "=", firstInput * i)
+        }
+    }
+}
+multiply(process.argv[2]);
+
+
+
+function addition(number) {
+    var firstInput = parseFloat(number)
+    
+    if (Number.isInteger(firstInput) == false) {
+        console.log("error");
+    } else {
+        for (var i = 1; i <= 10; i++) {
+            console.log(firstInput, "+", i, "=", firstInput + i)
+        }
+    }
+}
+addition(process.argv[3]);
