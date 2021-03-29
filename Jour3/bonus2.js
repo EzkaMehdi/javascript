ce63922febbbb27e6f0b3a8c9ccf6ee974c0755c
Mehdi <ezka.mehdi@gmail.com>
2021-03-29 17:17:47 +0200

    revisions

---
+++ Jour3/bonus2.js
@@ -0,0 +1,16 @@
+// - Créez une variable `total` de valeur `0` et une variable `limit` de valeur `10`
+// - À l'aide d'une boucle for allant de`0` à `limit`, augmentez la valeur de `total` en lui ajoutant la valeur de `i` (i est l'index de votre boucle)
+// - Affichez la valeur de `total` après votre boucle, total doit valoir 55 = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+// - Reproduisez l'exercice 5 avec une boucle while
+
+
+var total = 0
+var limit = 10
+// for (var i = 0; i <= limit; i++) {
+//     total = total + i;  
+   
+//   }
+//   console.log(total);
+while(i = 0){
+
+}
